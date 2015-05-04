ea40124861d6795dc210634e96e2a28ff40b5569
Jérôme Dohrau <dohrauj@student.ethz.ch>
2015-05-04 09:47:34 +0200

    Added comments

--- src/functions.hpp
+++ src/functions.hpp
@@ -9,11 +9,16 @@
 
 typedef std::vector<std::vector<int> > Graph;
 
+// computes the distances for all vertices to the specified vertex
 void distance_list(const Graph& graph, int vertex, std::vector<int>& distances);
+
+// computes the distances for all vertices to the set of specified vertices.
 void distance_list(const Graph& graph, std::vector<int>& vertices, std::vector<int>& distances);
 
-// computes a histogram of the shortest distances to one of the specified vertices
+// computes a histogramm of the distances to the specified vertex
 void distance_histogram(const Graph& graph, int vertex, std::vector<int>& histogram);
+
+// computes a histogram of the distances to one of the set of specified vertices
 void distance_histogram(const Graph& graph, std::vector<int>& vertices, std::vector<int>& histogram);
 
 // computes the eccentricity of the vertex with the specified index in the given graph
