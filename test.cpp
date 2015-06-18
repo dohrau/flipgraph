6cb6fb65be605110bd73b51525cf12ae08e8c3d4
Jérôme Dohrau <dohrauj@student.ethz.ch>
2015-06-18 11:13:09 +0200

    test.cpp modified

--- test.cpp
+++ test.cpp
@@ -4,21 +4,67 @@
 
 #include <iostream>
 #include "src/triangulation.hpp"
+#include "src/flipgraph.hpp"
+#include "src/functions.hpp"
+
+/* ---------------------------------------------------------------------- *
+ * helper functions
+ * ---------------------------------------------------------------------- */
+
+void print_list(std::vector<int> &list) {
+    int l = (int) list.size();
+    for (int i = 0; i < l; ++i) {
+        std::cout << list[i] << " ";
+    }
+    std::cout << std::endl;
+}
 
 /* ---------------------------------------------------------------------- *
  * main function
  * ---------------------------------------------------------------------- */
 
 int main(int argc, char *argv[]) {
-    int n = 6;
+    int n = 13;
 
-    Triangulation triangulation(n, TRIANGULATION_DOMINANT_DOUBLE_FAN);
-    Code code(triangulation);
+    Triangulation double_fan(n, TRIANGULATION_DOMINANT_DOUBLE_FAN);
+    Code double_fan_code(double_fan);
 
-    triangulation = *(new Triangulation(code));
+    Triangulation binary_tree(n, TRIANGULATION_DOMINANT_BINARY_TREE);
+    Code binary_tree_code(binary_tree);
 
-    //code.write_to_stream(std::cout);
-    triangulation.write_to_stream(std::cout);
+    FlipGraph flip_graph;
+    flip_graph.compute(n);
+
+    FlipGraph::Graph graph = flip_graph.graph();
+    int size = graph.size();
+
+    std::vector<int> bounds;
+    std::vector<int> histogram;
+
+    for (int i = 0; i < size; ++i) {
+        distance_histogram(graph, i, histogram);
+
+        int distance = (int) histogram.size() - 1;
+        int last = histogram[distance];
+        int bound = (last == 1) ? 2 * distance - 1 : 2 * distance;
+
+        if (bounds.size() <= bound) {
+            bounds.resize(bound + 1, 0);
+        }
+        bounds[bound]++;
+
+        if (i == 0) {
+            std::cout << "canonical : " << bound << std::endl;
+        }
+        if (flip_graph.code(i) == double_fan_code) {
+            std::cout << "double fan: " << bound << std::endl;
+        } else if (flip_graph.code(i) == binary_tree_code) {
+            std::cout << "binary tree: " << bound << std::endl;
+        }
+    }
+
+    std::cout << bounds.size() - 1 << std::endl;
+    print_list(bounds);
 
     return 0;
 }
